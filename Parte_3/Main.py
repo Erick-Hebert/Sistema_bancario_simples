3bb1c5b9a924b85cda1f770c1471d8047e66590a
Erickfontes <124408907+Erick-Hebert@users.noreply.github.com>
2025-08-12 18:42:17 -0300

    Parte 3 finalizada

--- Parte_3/Main.py
+++ Parte_3/Main.py
@@ -12,6 +12,10 @@ class Cliente:
     def Adicionar_conta(self, conta):
         self._contas.append(conta)
         
+    @property
+    def contas(self):
+        return self._contas
+        
 class Pessoa_fisica(Cliente):
     def __init__(self, endereco, cpf, nome, nascimento):
         super().__init__(endereco)
@@ -21,6 +25,22 @@ class Pessoa_fisica(Cliente):
         
     def __str__(self):
         return f'\nNome: {self._nome}'
+    
+    @property
+    def cpf(self):
+        return self._cpf
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def nascimento(self):
+        return self._nascimento
+    
+    @property
+    def endereco(self):
+        return self._endereco
 
 class Conta:
     def __init__(self, numero, cliente):
@@ -105,7 +125,7 @@ class Historico:
         self._transacoes = []
         
     @property
-    def transacoes(self):
+    def Transacoes(self):
         return self._transacoes
         
     
@@ -114,6 +134,10 @@ class Historico:
             'tipo' : transacao.__class__.__name__,
             'valor' : transacao.valor,
             'data' : datetime.now().strftime('%d-%m-%Y %H:%M:%S')})
+        
+    @property
+    def transacoes(self):
+        return self._transacoes
 
 class Transacao(ABC):
     
@@ -154,7 +178,7 @@ class Deposito(Transacao):
         if transacao_efetuada:
             conta.Historico.Adicionar_transacao(self)    
 
-def Titulo(mensagem):
+def Titulo(mensagem=''):
         print(f'\n{mensagem:=^30}')
     
 def Menu():
@@ -165,22 +189,21 @@ def Menu():
     3 - Extrato
     4 - Cadastrar Usuário
     5 - Cadastrar Conta
-    6 - Listar Usuários
-    7 - Listar Contas
-    8 - Sair
+    6 - Listar Contas
+    7 - Sair
     ''');
 
 def Buscar_cliente(cpf, clientes):
     cliente = [cliente for cliente in clientes if cliente.cpf == cpf]
     return cliente[0] if cliente else None
     
-def Buscar_conta(cliente):
-    if not cliente.contas:
-        print('\nO usuário não possui conta alguma')
-        return None
+def Buscar_conta(cliente, numero):
+    for conta in cliente.contas:
+        if conta.Numero == numero:
+            return conta
+    print('\nO usuário não possui a conta informada')
+    return None
          
-    return cliente.contas[0]
-
 def Depositar(clientes):
     cpf = input('Informe o cpf do cliente: ').strip()
     cliente = Buscar_cliente(cpf, clientes)
@@ -189,16 +212,98 @@ def Depositar(clientes):
         print('\nCliente não encontrado')
         return
     
+    conta_existe = int(input('Informe o número da conta: ').strip())
+    conta = Buscar_conta(cliente, conta_existe)
+    
+    if not conta:
+        return 'A conta informada não foi encontrada'
+    
+       
     valor = float(input('Informe o valor a ser depositado: '))
     transacao = Deposito(valor)
     
-    conta = Buscar_conta(cliente)
+    cliente.Realizar_transacao(conta, transacao)
+    
+def Sacar(clientes):
+    cpf = input('Informe o cpf do cliente: ').strip()
+    cliente = Buscar_cliente(cpf, clientes)
+    
+    if not cliente:
+        print('\nCliente não encontrado')
+        return
+    
+    conta_existe = int(input('Informe o número da conta: ').strip())
+    conta = Buscar_conta(cliente, conta_existe)
     
     if not conta:
-        return
+        return 'A conta informada não foi encontrada'
+    
+    
+    valor = float(input('Informe o valor a ser sacado: '))
+    transacao = Saque(valor)
     
     cliente.Realizar_transacao(conta, transacao)
     
+def Extrato(clientes):
+    cpf = input('Informe o cpf do cliente: ').strip()
+    cliente = Buscar_cliente(cpf, clientes)
+    
+    if not cliente:
+        print('\nCliente não encontrado')
+        return
+    
+    conta_existe = int(input('Informe o número da conta: ').strip())
+    conta = Buscar_conta(cliente, conta_existe)
+    
+    if not conta:
+        return 'A conta informada não foi encontrada'
+    
+    Titulo('Extrato')
+    
+    transacoes = conta.Historico.Transacoes
+    extrato = ''
+    if not transacoes:
+        print('\nNão foram realizadas transações!')
+    else:
+        for transacao in transacoes:
+            extrato += f'\n{transacao['tipo']}:\n\tR${transacao['valor']:.2f}'
+    
+    print(extrato)
+    print(f'\nSaldo:\n\t{conta.Saldo:.2f}')
+    Titulo()
+    
+def Criar_conta(numero, clientes, contas):
+    cpf = input('Informe o cpf do cliente: ').strip()
+    cliente = Buscar_cliente(cpf, clientes)
+    
+    if not cliente:
+        print('\nCliente não encontrado')
+        return
+    
+    conta = Conta_Corrente.Nova_conta(cliente, numero)
+    contas.append(conta)
+    cliente.contas.append(conta)
+    print('\nConta criada com sucesso')
+        
+def Criar_cliente(clientes):
+    cpf = input('Informe o cpf do cliente: ').strip()
+    cliente = Buscar_cliente(cpf, clientes)  
+    if cliente:
+        print('\nO cliente já está cadastrado')
+        return
+    
+    nome = input('Informe o nome do cliente: ').strip()
+    nascimento = input('Informe o nascimento do cliente: ').strip()
+    endereco = input('Informe o endereço do cliente: ').strip()
+    
+    cliente = Pessoa_fisica(endereco, cpf, nome, nascimento);
+    clientes.append(cliente);
+    
+def Listar_contas(contas):
+    Titulo('Listagem de Contas')
+    for conta in contas:
+        print(conta)
+                        
 class Main():
     clientes = []
     contas = []
@@ -210,3 +315,16 @@ class Main():
         match opcao:
             case '1':
                 Depositar(clientes)
+            case '2':
+                Sacar(clientes)
+            case '3':
+                Extrato(clientes)
+            case '4':
+                Criar_cliente(clientes)
+            case '5':
+                Criar_conta(len(contas)+1, clientes, contas)
+            case '6':
+                Listar_contas(contas)    
+            case '7':
+                break
+                
